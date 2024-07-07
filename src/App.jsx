6b442ccf5dcd656032d21e3b5d9d0cc0581824dd
jcguana <jcguana@puce.edu.ec>
2024-07-07 14:04:59 -0500

    Creacion de las paginas de Registro y cuestionario tanto .jsx como .css y conexion mediante react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+import Cuestionario from './pages/Cuestionario';  // Asegúrate de que esta ruta es correcta
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const navigate = useNavigate();
+
+  const handleCreateUser = () => {
+    navigate('/cuestionario');
+  };
 
   return (
     <>
-      <div>
-        <img src="" alt="" />
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <div>
+              <img src="/images/logo.png" alt="Logo" />
+            </div>
+            <div className='Usuario'>
+              <p>Usuario:</p>
+              <input type="text" />
+            </div>
+            <div className='Contraseña'>
+              <p>Contraseña:</p>
+              <input type="text" />
+            </div>
+            <div className='Buttons'>
+              <button onClick={handleCreateUser}>Create User</button>
+              <button>Log In</button>
+            </div>
+          </div>
+        } />
+        <Route path="/cuestionario" element={<Cuestionario />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+ 
