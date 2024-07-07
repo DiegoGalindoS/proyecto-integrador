566ff8c74d49f7fe49221c6e5336c966a3dd393a
WeeZ4343 <168301038+DiegoGalindoS@users.noreply.github.com>
2024-07-07 04:10:16 -0500

    Creación de MyList
    creacion del componente mylist.jsv y css

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';  // Importa los estilos globales
 import Header from './Header';
 import BasicButtons from './button';  // Asegúrate de tener el archivo Boton.jsx o Boton.js en la misma carpeta
+import MyList from './to do list/myList.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <BasicButtons />
+    
+    <MyList />
   </React.StrictMode>,
   document.getElementById('root')
 );
